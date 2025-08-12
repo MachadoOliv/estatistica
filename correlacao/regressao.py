b704e92cd3017482754084aa55c7ec27f535920c
Moisés de Oliveira <moisesmachado7078@gmail.com>
2025-08-11 21:21:51 -0300

    Update regressao.py

--- correlacao/regressao.py
+++ correlacao/regressao.py
@@ -11,3 +11,25 @@ print(dataframe.head())
 
 dataframe.plot.scatter(x= 'Exposure', y='PEFR')
 plt.show() 
+
+#3. Configuração e treinamento do modelo
+#Define a variável preditora (independente), que é a Exposure e a variável de resultados que é o PEFR
+predictors = ['Exposure']
+outcome = 'PEFR'
+#Instanciar o modelo
+model = LinearRegression()
+#Etapa de treinamento 
+model.fit(dataframe[predictors], dataframe[outcome])
+
+#4. Exibição dos Coeficientes
+#Intercepto
+print(f'Intercepto: {model.intercept_:.3f}')
+#Coeficiente Angular
+print(f'Coeficiente Angular: {model.coef_[0]}')
+
+#5. Geração do gráfico
+fig,(reg) = plt.subplots(1,1, figsize = (4,4))
+#Gráfico regreção
+reg = sns.regplot (x = 'Exposure', y = 'PEFR', data = dataframe, ax = reg)
+plt.tight_layout()
+plt.show()
